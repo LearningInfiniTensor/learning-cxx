a8949ec8c1f2ec3f6ea89fd61a2cb4177132b8e2
YdrMaster <ydrml@hotmail.com>
2024-07-15 11:53:24 +0800

    update: 完成 26 题
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- exercises/26_std_string/main.cpp
+++ exercises/26_std_string/main.cpp
@@ -1,7 +1,18 @@
 ﻿#include "../exercise.h"
 #include <string>
 
+// READ: 字符串 <https://zh.cppreference.com/w/cpp/string/basic_string>
+
 int main(int argc, char **argv) {
-    ASSERT(false, "todo!");
+    // READ: 字符串字面量 <https://zh.cppreference.com/w/cpp/string/basic_string/operator%22%22s>
+    using namespace std::string_literals;
+    auto hello = "Hello"s;
+    auto world = "world";
+    // READ: `decltype` 表达式 <https://zh.cppreference.com/w/cpp/language/decltype>
+    // READ: `std::is_same_v` 元编程判别 <https://zh.cppreference.com/w/cpp/types/is_same>
+    ASSERT((std::is_same_v<decltype(hello), ?>), "Fill in the missing type.");
+    ASSERT((std::is_same_v<decltype(world), ?>), "Fill in the missing type.");
+    // TODO: 将 `?` 替换为正确的字符串
+    ASSERT(hello + ", " + world + '!' == "?", "Fill in the missing string.");
     return 0;
 }
