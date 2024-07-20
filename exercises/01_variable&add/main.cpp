ba99b5b5b08508f9ced730f9286498eec2eab606
YdrMaster <ydrml@hotmail.com>
2024-07-20 15:15:50 +0800

    update: 增加阅读材料
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- exercises/01_variable&add/main.cpp
+++ exercises/01_variable&add/main.cpp
@@ -1,5 +1,7 @@
 #include "../exercise.h"
 
+// READ: 运算符 <https://zh.cppreference.com/w/cpp/language/expressions#.E8.BF.90.E7.AE.97.E7.AC.A6>
+
 int main(int argc, char **argv) {
     // TODO: 补全变量定义并打印加法运算
     // x ?
