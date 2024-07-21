6f8d906708bccf190b227542955fb3055f0188d8
chenyinheng30 <chenyinheng30@outlook.com>
2024-07-21 10:10:12 +0800

    解决由于类型比较两边类型不一致的导致的警告

--- exercises/06_loop/main.cpp
+++ exercises/06_loop/main.cpp
@@ -3,7 +3,8 @@
 // TODO: 改正函数实现，实现正确的缓存优化斐波那契计算
 static unsigned long long fibonacci(int i) {
     // TODO: 为缓存设置正确的初始值
-    static unsigned long long cache[128], cached = 2;
+    static unsigned long long cache[128];
+    int cached = 2;
     cache[0] = 0;
     cache[1] = 1;
     // TODO: 设置正确的循环条件
