9d989a0c87b4e8ed7976c49ca96217631ddb2784
chenyinheng30 <chenyinheng30@outlook.com>
2024-07-21 11:09:20 +0800

    完成 21 题

--- exercises/21_template_const/main.cpp
+++ exercises/21_template_const/main.cpp
@@ -1,4 +1,5 @@
 ﻿#include "../exercise.h"
+#include <cstring>
 
 // READ: 模板非类型实参 <https://zh.cppreference.com/w/cpp/language/template_parameters#%E6%A8%A1%E6%9D%BF%E9%9D%9E%E7%B1%BB%E5%9E%8B%E5%AE%9E%E5%8F%82>
 
@@ -10,6 +11,10 @@ struct Tensor {
     Tensor(unsigned int const shape_[N]) {
         unsigned int size = 1;
         // TODO: 填入正确的 shape 并计算 size
+        for (unsigned i = 0; i < N; i++) {
+            size *= shape_[i];
+            shape[i] = shape_[i];
+        }
         data = new T[size];
         std::memset(data, 0, size * sizeof(T));
     }
@@ -32,9 +37,11 @@ private:
     unsigned int data_index(unsigned int const indices[N]) const {
         unsigned int index = 0;
         for (unsigned int i = 0; i < N; ++i) {
-            ASSERT(indices[i] < shape[i]);
+            ASSERT(indices[i] < shape[i], "Index out of range.");
             // TODO: 计算 index
+            index = shape[i] * index + indices[i];
         }
+        return index;
     }
 };
 
@@ -49,8 +56,8 @@ int main(int argc, char **argv) {
         ASSERT(tensor[i0] == 1, "tensor[i0] should be 1");
 
         unsigned int i1[]{1, 2, 3, 4};
-        tensor[i0] = 2;
-        ASSERT(tensor[i0] == 2, "tensor[i1] should be 2");
+        tensor[i1] = 2;
+        ASSERT(tensor[i1] == 2, "tensor[i1] should be 2");
     }
     {
         unsigned int shape[]{7, 8, 128};
@@ -61,8 +68,8 @@ int main(int argc, char **argv) {
         ASSERT(tensor[i0] == 1.f, "tensor[i0] should be 1");
 
         unsigned int i1[]{3, 4, 99};
-        tensor[i0] = 2.f;
-        ASSERT(tensor[i0] == 2.f, "tensor[i1] should be 2");
+        tensor[i1] = 2.f;
+        ASSERT(tensor[i1] == 2.f, "tensor[i1] should be 2");
     }
     return 0;
 }
