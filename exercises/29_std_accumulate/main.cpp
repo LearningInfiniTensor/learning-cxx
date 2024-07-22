5d106954776514c0f6d44b9e87fe557fb8350403
YdrMaster <ydrml@hotmail.com>
2024-07-22 11:23:53 +0800

    fix(exercise): 改正 29
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- exercises/29_std_accumulate/main.cpp
+++ exercises/29_std_accumulate/main.cpp
@@ -6,8 +6,12 @@
 int main(int argc, char **argv) {
     using DataType = float;
     int shape[]{1, 3, 224, 224};
-    // TODO: 调用 `std::accumulate` 计算 `shape` 的元素之积
+    // TODO: 调用 `std::accumulate` 计算：
+    //       - 数据类型为 float；
+    //       - 形状为 shape；
+    //       - 连续存储；
+    //       的张量占用的字节数
     // int size =
-    ASSERT(size = 602112, "4x1x3x224x224 = 602112");
+    ASSERT(size == 602112, "4x1x3x224x224 = 602112");
     return 0;
 }
