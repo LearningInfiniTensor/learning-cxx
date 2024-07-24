01b6ce59147db70489d1185109aa748f4db209f3
YdrMaster <ydrml@hotmail.com>
2024-07-24 10:59:43 +0800

    fix(exercise): 21 assert 写错
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- exercises/21_template_const/main.cpp
+++ exercises/21_template_const/main.cpp
@@ -33,9 +33,10 @@ private:
     unsigned int data_index(unsigned int const indices[N]) const {
         unsigned int index = 0;
         for (unsigned int i = 0; i < N; ++i) {
-            ASSERT(indices[i] < shape[i]);
+            ASSERT(indices[i] < shape[i], "Invalid index");
             // TODO: 计算 index
         }
+        return index;
     }
 };
 
@@ -48,10 +49,12 @@ int main(int argc, char **argv) {
         unsigned int i0[]{0, 0, 0, 0};
         tensor[i0] = 1;
         ASSERT(tensor[i0] == 1, "tensor[i0] should be 1");
+        ASSERT(tensor.data[0] == 1, "tensor[i0] should be 1");
 
         unsigned int i1[]{1, 2, 3, 4};
-        tensor[i0] = 2;
-        ASSERT(tensor[i0] == 2, "tensor[i1] should be 2");
+        tensor[i1] = 2;
+        ASSERT(tensor[i1] == 2, "tensor[i1] should be 2");
+        ASSERT(tensor.data[119] == 2, "tensor[i1] should be 2");
     }
     {
         unsigned int shape[]{7, 8, 128};
@@ -60,10 +63,12 @@ int main(int argc, char **argv) {
         unsigned int i0[]{0, 0, 0};
         tensor[i0] = 1.f;
         ASSERT(tensor[i0] == 1.f, "tensor[i0] should be 1");
+        ASSERT(tensor.data[0] == 1.f, "tensor[i0] should be 1");
 
         unsigned int i1[]{3, 4, 99};
-        tensor[i0] = 2.f;
-        ASSERT(tensor[i0] == 2.f, "tensor[i1] should be 2");
+        tensor[i1] = 2.f;
+        ASSERT(tensor[i1] == 2.f, "tensor[i1] should be 2");
+        ASSERT(tensor.data[3683] == 2.f, "tensor[i1] should be 2");
     }
     return 0;
 }
