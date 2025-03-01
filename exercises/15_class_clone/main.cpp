1c2735a3121e7d787596abc66fbc35c89a297d4b
xgqdut2016 <kenan_gewei@163.com>
2025-03-01 22:47:45 +0800

    success 15

--- exercises/15_class_clone/main.cpp
+++ exercises/15_class_clone/main.cpp
@@ -10,13 +10,17 @@ class DynFibonacci {
 
 public:
     // TODO: 实现动态设置容量的构造器
+    //构造函数使用初始化列表来分配cache数组并初始化cached成员是非常重要的
     DynFibonacci(int capacity): cache(new size_t[capacity]), cached(2) {
         cache[0] = 0;//通过构造函数Fibonacci把cache前两个初始化为0，1
         cache[1] = 1;
     }
 
     // TODO: 实现复制构造器
-    DynFibonacci(DynFibonacci const &other) : cache(other.cache), cached(other.cached){
+    DynFibonacci(DynFibonacci const &other): cache(other.cache), cached(other.cached) {
+        //cached = other.cached;
+        //cache = new size_t[cached];//最好还是申请一片新内存来存放新对象的cache
+        //这种时候可能也需要额外复制析构器释放缓存空间
         for(int i = 0; i < cached; i++){//通过cache(other.cache)复制一个新的数组，通过cached(other.cached)把other的cached赋值给cached
             cache[i] = other.cache[i];
         }//此时要求第一个类必须先跑一次get函数，把cached结果更新才能保证正确性
@@ -39,7 +43,7 @@ public:
     // NOTICE: 名字相同参数也相同，但 const 修饰不同的方法是一对重载方法，可以同时存在
     //         本质上，方法是隐藏了 this 参数的函数
     //         const 修饰作用在 this 上，因此它们实际上参数不同
-    size_t get(int i) const {
+    size_t get(int i) const {//这个const的存在使得这个get函数不能修改cache数组
         if (i <= cached) {
             return cache[i];
         }
